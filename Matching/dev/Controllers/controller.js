fd5d0df2c55d7ec8fb3302730a6471418855bbcf
Víctor del Cid <victordelcidr@gmail.com>
2018-09-06 21:24:04 -0600

    Comenté los errores extras a targeting

--- Matching/dev/Controllers/controller.js
+++ Matching/dev/Controllers/controller.js
@@ -2,42 +2,70 @@ var express = require('express');
 var con = require('../Models/model.js');
 
 
-exports.findMatch = function(req, res){
-  let category = req.query.category;
-  console.log(req.query.category);
-  
-  if (!category) {
-      console.log("No Category");
-      res.status(400).json({
-          status: 400,
-          message: "No Category"
-      })
-      return;
-  }
-  con.query('SELECT id, bid, targeting FROM advertiser_campaigns WHERE status = true AND category = ? ', category, (err, result, fields) => {
-      if (err) {
-          console.log("Status 500. Details: " + err);
-          res.status(500).json({
-              status: 500,
-              message: "Internal server error",
-              details: err
-          });
-          return;
-      } 
-      if (result.length > 0){
-          console.log(JSON.stringify(result, null, 4));
-          res.status(200).json({
-              results: result
-          });
-      }
-      else {
-          console.log("No existe ninguna campaña con el ID " + category + ".");
-          res.status(404).json({
-              status: 404,
-              message: "No existe ninguna campaña con el ID " + " #" + category + "."
-          });
-      }
+exports.findsQuery = function (req, res) {
+    let category = req.query.category;
+    let publisher_campaign = req.query.publisher_campaign;
+    let zip_code = req.query.zip_code;
+    let maximum = req.query.maximum;
 
-  });
+    console.log(req.query.category);
+    console.log(req.query.publisher_campaign);
+    console.log(req.query.zip_code);
 
+
+    if (!category) {
+        console.log("No Category");
+        res.status(400).json({
+            status: 400,
+            message: "No Category"
+        })
+        return;
+    }
+    /*
+    if (!publisher_campaign) {
+        console.log("No Campaign");
+        res.status(400).json({
+            status: 400,
+            message: "No campaign"
+        })
+        return;
+    }
+
+    if (!zip_code) {
+        console.log("No Zip code");
+        res.status(400).json({
+            status: 400,
+            message: "No zip code"
+        })
+        return;
+    }
+    */
+    https.get('localhost:3002?category=' + category, (resp) => {
+        let data = '';
+        // A chunk of data has been recieved
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        // The whole response has been received
+        resp.on('end', () => {
+            console.log("Response: " + data);
+            res.send('JSON: ' + data);
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    }); 
+
+    /*https.get('localhost:3001?advertiser_campaigns=429&publisher_campaign=73=' + category, (resp) => {
+        let data = '';
+        // A chunk of data has been recieved
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        // The whole response has been received
+        resp.on('end', () => {
+
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    }); */
 }
