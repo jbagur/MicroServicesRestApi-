bc93e6eb8723255625ef39e8e0e5ec6f60c3a9ac
jbagur <rafa@rafa-Lenovo-YOGA-900-13ISK>
2018-09-06 20:21:03 -0600

    pricing

--- Pricing/dev/Controllers/controller.js
+++ Pricing/dev/Controllers/controller.js
@@ -3,15 +3,29 @@ var con = require('../Models/model.js');
 
 
 exports.findPricing= function(req, res){
-
-  let advertiser_campaigns = req.query.advertiser_campaigns;
-  console.log(req.query.advertiser_campaign); 
     
-  let advertiser_campaigns_bids = req.query.advertiser_campaigns_bids;
-  console.log(req.query.advertiser_campaigns_bids);
+  let advertiser_campaigns = req.query.advertiser_campaigns.split(",");
+  //console.log("Lista de Advertiser Id's");
+  //console.log(advertiser_campaigns);
+    
+  let advertiser_campaigns_bids = req.query.advertiser_campaigns_bids.split(",");
+  //console.log("Lista de Bid's");
+  //console.log(advertiser_campaigns_bids);
 
   let publisher_campaign = req.query.publisher_campaign;
-  console.log(req.query.publisher_campaign);
+  //console.log(req.query.publisher_campaign);
+
+  let adCamId = [];
+  let bidCom = [];
+  for(i=0; i<advertiser_campaigns.length; i++){
+      adCamId.push(parseInt(advertiser_campaigns[i]));
+      bidCom.push(parseInt(advertiser_campaigns_bids[i]));
+  }
+  
+  //console.log("Listas");
+  //console.log(adCamId);
+  //console.log(bidCamId);
+  
   
    if (!advertiser_campaigns) {
       console.log("No Advertiser Campaign");
@@ -50,9 +64,29 @@ exports.findPricing= function(req, res){
           return;
       } 
       if (result.length > 0){
-          console.log(JSON.stringify(result, null, 4));
+          let itemJson = JSON.stringify(result);
+          let itemParsed = JSON.parse(itemJson);
+          //console.log("itemJson");
+          //console.log(itemJson);
+          //console.log("itemParsed");
+          //console.log(itemParsed);
+          //console.log(itemJson[0]["commission"]);
+          let commVal = itemParsed[0]["commission"]
+          //console.log(commVal);
+          //console.log(bidCom);
+          for(i=0; i<bidCom.length; i++){
+            bidCom[i]*=commVal;
+        }
+          //console.log(bidCom);
+
+          var obj = {};
+          for (var i = 0; i < bidCom.length; i++) {
+             obj[adCamId[i]] = bidCom[i];
+          }
+
+          console.log(JSON.stringify(obj, null, 4));
           res.status(200).json({
-              results: result
+              results: obj
           });
       }
       else {
