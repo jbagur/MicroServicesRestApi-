ba084f43f6953cddb2d25089c9e662a2d711a344
Víctor del Cid <victordelcidr@gmail.com>
2018-09-09 14:20:54 -0600

    Testing get function

---
+++ Query/controller.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var con = require('../Models/model.js');
+
+
+exports.findQuery.get('/', function (req, res) {
+    let category = req.query.category;
+    let publisher_campaign = req.query.publisher_campaign;
+    let zip_code = req.query.zip_code;
+    let maximum = req.query.maximum;
+
+    console.log("Category: "+category+" Publisher_campaign: " +publisher_campaign+" Zip code: " +zip_code+" Maximum: "+maximum);
+
+
+    if (!category) {
+        console.log("No Category");
+        res.status(400).json({
+            status: 400,
+            message: "No Category"
+        })
+        return;
+    }
+    
+    if (!publisher_campaign) {
+        console.log("No Campaign");
+        res.status(400).json({
+            status: 400,
+            message: "No campaign"
+        })
+        return;
+    }
+
+    if (!zip_code) {
+        console.log("No Zip code");
+        res.status(400).json({
+            status: 400,
+            message: "No zip code"
+        })
+        return;
+    }
+    
+    https.get('18.212.105.67:3001/?category=' + category, (resp) => {
+        let data = '';
+        // A chunk of data has been received
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        // The whole response has been received
+        resp.on('end', () => {
+            console.log("Response: " + data);
+            res.send('JSON: ' + data);
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    }); 
+
+    /*https.get('localhost:3001?advertiser_campaigns=429&publisher_campaign=73=' + category, (resp) => {
+        let data = '';
+        // A chunk of data has been recieved
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        // The whole response has been received
+        resp.on('end', () => {
+
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    }); */
+})
